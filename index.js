77fdb1421bc8617666068ce7fa9fb4a97490397a
Kiiroi <sammydevjs@gmail.com>
2022-07-16 05:56:00 -0300

    Letra de fonte maxima definida

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+// fazer botao responder ao enter do teclado
 // O nome do chat tem que ser com letra maiúscula no começo também
 // ONLINE TEM QUE VIRAR 3 PONTOS SEMPRE QUE FOR EXECUTAR O PRÓXIMO CHAT DA PESSOA 
 // fazer os baloes de chat vir um a um
+
 
 
 // Salvei o botao na constante botao
