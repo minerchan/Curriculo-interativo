a26f57493b0cd262920566c21aba6acbafa50f11
Kiiroi <sammydevjs@gmail.com>
2022-07-18 08:48:23 -0300

    Reforma no css, pagina da frente, nome e zoom funcionando

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 2.2 é o mínimo 2.7VH é o máximo da fonte
+// Nome nao pode ter mais que 30 letras
 // DESABILITAR O ZOOM E PERMITIR SÓ ENTRE 100% E 160%
 // SE CAIXA DE TEXTO TIVER VAZIO RETORNAR "POR FAVOR COLOQUE SEU NOME"
 // FAZER O BOTAO DE POR O NOME RESPONDER AO TECLADO
@@ -7,33 +7,61 @@
 // CONVERSA - INSERIR TEMPO UNIFORME DE ESPERA ENTRE CHATS NUMA FUNÇÃO. USAR ELA SEMPRE ANTES DA EXIBIÇÃO DE UMA CAIXA DE MENSAGEM 
 
 // Coloquei todos os chats numa lista e usei ela para mudar o tamanho da fonte de todos os chats, se chegar ao máximo ele loga
+// DEIXA AQUI PARA QUANDO EU DESCOBRIR COMO MANIPULAR ISSO
 
+
+// AQUI É ONDE DEFINE O ZOOM MÁXIMO E ZOOM MÍNIMO
+const ListaMais = document.querySelector(".mais");
+const ListaMenos = document.querySelector(".menos");
 const ListaDeChat = document.querySelectorAll('.chat');
-for (let index = 0; index < ListaDeChat.length; index++) {
-  let tamanhoDaFonte = ListaDeChat[index].style.fontSize = "2.2vh";
-  if(tamanhoDaFonte === "2.7vh"){
-  console.log(tamanhoDaFonte)}
-}
-
-// aqui eu vou fazer o botao de aumentar e diminuir fonte
+let numeroFonteA = 2.2;
 
 
+ListaMais.addEventListener('click', function(){
+  if(numeroFonteA >= 2.7){
+    ListaMais.style.backgroundColor = "#A27B5C";
+  } else{
+    ListaMenos.style.backgroundColor = "#DCD7C9";
+    numeroFonteA = numeroFonteA + 0.1;
+    numeroFonteA.toFixed(1);
+    ListaDeChat[0].style.fontSize = `${numeroFonteA}vh`
+    ListaDeChat[1].style.fontSize = `${numeroFonteA}vh`
+    ListaDeChat[2].style.fontSize = `${numeroFonteA}vh`
+    ListaDeChat[3].style.fontSize = `${numeroFonteA}vh`
+    ListaDeChat[4].style.fontSize = `${numeroFonteA}vh`
+    ListaDeChat[5].style.fontSize = `${numeroFonteA}vh`
+  }
+})
+ListaMenos.addEventListener('click', function(){
+  
+  if(numeroFonteA < 2.2){
+    ListaMenos.style.backgroundColor = "#A27B5C";
+    }
+    else{
+    ListaMenos.style.backgroundColor = "#DCD7C9";
+    ListaMais.style.backgroundColor = "#DCD7C9";
+    numeroFonteA = numeroFonteA - 0.1;
+    numeroFonteA.toFixed(1);
+    ListaDeChat[0].style.fontSize = `${numeroFonteA}vh`
+    ListaDeChat[1].style.fontSize = `${numeroFonteA}vh`
+    ListaDeChat[2].style.fontSize = `${numeroFonteA}vh`
+    ListaDeChat[3].style.fontSize = `${numeroFonteA}vh`
+    ListaDeChat[4].style.fontSize = `${numeroFonteA}vh`
+    ListaDeChat[5].style.fontSize = `${numeroFonteA}vh`
+    }
+  })
 
-
-
-// Salvei o botao na constante botao
+// Nessa funcao quando clica no botao ele salva o texto, troca a variavel pro texto e tira o esconder do chat
 
 const botao = document.querySelector("#botao");
 
-
-// Nessa funcao quando clica no botao ele salva o texto, troca a variavel pro texto e tira o esconder do chat
 botao.addEventListener('click', ()=>{
   let nome = "";
   nome = document.querySelector("#caixa").value
   if(nome === ""){
     console.log("funciona")
   } else {
-  document.querySelector('#gridchat').style.display = "none";
+  document.querySelector('#textoalin').style.display = "none";
   document.querySelector('.conversa').classList.remove("escondido");
   document.querySelector("#nomeDaPessoa").textContent = nome;
   document.querySelector("#variavel").textContent = nome;
